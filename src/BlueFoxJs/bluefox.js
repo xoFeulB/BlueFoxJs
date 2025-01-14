597e7e7e7d6d9201861128dd8f1b0c91eb36fefa
はまち <31212444+HeyHamachi@users.noreply.github.com>
2025-01-15 05:13:44 +0900

    Split script

--- src/BlueFoxJs/bluefox.js
+++ src/BlueFoxJs/bluefox.js
@@ -1,30 +1,20 @@
-import { BlueFox, BlueFoxScript } from "@xofeulb/bluefox-js/Automation/BlueFox.js";
+import { deepFreeze } from "@xofeulb/bluefox-js/Util/DeepFreeze.js";
 import { getProperty } from "@xofeulb/bluefox-js/Util/GetProperty.js";
 import { getAllPath } from "@xofeulb/bluefox-js/Util/JSON.js";
 import { walkHorizontally } from "@xofeulb/bluefox-js/Walker/WalkHorizontally.js";
 import { walkVertically } from "@xofeulb/bluefox-js/Walker/WalkVertically.js";
-import { view, enableSyncViewElement } from "@xofeulb/bluefox-js/Sync/View.js";
-import { value } from "@xofeulb/bluefox-js/Sync/Value.js";
 
 ("use strict");
 export const BlueFoxJs = (() => {
   let BlueFoxJs = {
-    Automation: {
-      BlueFox: BlueFox,
-      BlueFoxScript: BlueFoxScript,
-    },
     Util: {
+      deepFreeze: deepFreeze,
       getProperty: getProperty,
       getAllPath: getAllPath,
     },
     Walker: {
       walkHorizontally: walkHorizontally,
       walkVertically: walkVertically,
-    },
-    Sync: {
-      view: view,
-      enableSyncViewElement: enableSyncViewElement,
-      value: value,
     },
   };
   return BlueFoxJs;
